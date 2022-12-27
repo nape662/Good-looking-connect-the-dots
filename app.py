110c7d052ffd229640430caa7f918072baec7abe
nape662 <89686374+nape662@users.noreply.github.com>
2022-12-27 21:43:54 +0100

    fixed loop and spawn functionality

--- app.py
+++ app.py
@@ -1,21 +1,14 @@
 from dots import *
 
+
 # фишка класса это что если у тебя происходит все внутри одного объекта,
 # то его поля по факту глобальные и (self.x ~= global x)
 
 
 def get_square_coord(coords):
     x, y = coords
-    dotx = int((x - (SCREEN_WIDTH - 600) / 2) // 100)
-    doty = int((y - 175) // 100) # might change if we change window size
-    if dotx > 5:
-        dotx = 5
-    if doty > 5:
-        doty = 5
-    if dotx < 0:
-        dotx = 0
-    if doty < 0:
-        dotx = 0
+    dotx = max(min(int((x - (SCREEN_WIDTH - 600) / 2) // 100), 5), 0)
+    doty = max(min(int((y - 175) // 100), 5), 0)  # might change if we change window size
     return dotx, doty
 
 
@@ -33,7 +26,8 @@ class App:
         self.connected = list()
 
     def draw_line(self, new_dot):
-        pg.draw.line(self.screen, self.connected[-1].colour, self.connected[-1].rect.center, new_dot.rect.center, width=10)
+        pg.draw.line(self.screen, self.connected[-1].colour, self.connected[-1].rect.center, new_dot.rect.center,
+                     width=10)
         self.connected.append(new_dot)
 
     def shorten_line(self):
@@ -41,16 +35,14 @@ class App:
         self.connected.pop(-1)
 
     def handle_connected(self):
-    # ONLY FIRST LOOPED DOTS POP RIGHT!
         self.screen.fill(BG_COLOR)  # erases all lines
         if len(self.connected) > 1:
             if self.connected_has_loop():
                 loop_colour_number = self.connected[0].colour_number
-                for i in self.dots:
-                    for j in i:
-                        if j.colour_number == loop_colour_number:
-                            print(j.colour_number, loop_colour_number)
-                            j.pop(loop=True)  # can we make this less ugly?
+                for i in range(6):  # use in range to control order of popping dots
+                    for j in range(6):
+                        if self.dots[i][j].colour_number == loop_colour_number:
+                            self.dots[i][j].pop(in_loop=True)
             else:
                 for i in self.connected:
                     i.pop()
@@ -63,14 +55,20 @@ class App:
                     return True
         return False
 
+    def grid_has_pairs(self):
+        for i in range(6):
+            for j in range(5):
+                if self.dots[i][j].colour_number == self.dots[i][j+1] or self.dots[j][i] == self.dots[j+1][i]:
+                    return False
+        return True
+
     def handle_mouse(self, event):
         try:
             dotx, doty = get_square_coord(event.pos)
 
             if not self.connected:
                 self.connected.append(self.dots[dotx][doty])
-            elif self.dots[dotx][doty] != self.connected[-1] and self.dots[dotx][doty].colour == self.connected[
-                -1].colour:
+            elif self.dots[dotx][doty] != self.connected[-1] and self.dots[dotx][doty].colour_number == self.connected[-1].colour_number:
                 if (dotx == self.connected[-1].column and abs(doty - self.connected[-1].row) <= 1) or (
                         doty == self.connected[-1].row and abs(dotx - self.connected[-1].column <= 1)):
                     if len(self.connected) > 1 and self.dots[dotx][doty] == self.connected[-2]:
@@ -93,7 +91,7 @@ class App:
                     self.handle_mouse(event)
                 elif event.type == pg.MOUSEBUTTONUP:
                     self.handle_connected()
-                elif event.type == pg.KEYDOWN:
+                elif event.type == pg.KEYDOWN:  # cheat for testing
                     dotx, doty = get_square_coord(pg.mouse.get_pos())
                     self.dots[dotx][doty].pop()
 
--- dots.py
+++ dots.py
@@ -5,28 +5,27 @@ SCREEN_WIDTH = 650
 SCREEN_HEIGHT = 800
 BG_COLOR = (240, 228, 202, 255)
 COLOUR_LIST = [(125, 179, 73), (210, 106, 70), (243, 192, 58), (123, 85, 125), (90, 191, 211)]
+EXCLUDED_COLOR = None
 
 # inherit Sprite class with no real purpose tbh (in class Dot)
 # since I'm not using collision or group draw methods
 
 
 class Dot(pg.sprite.Sprite):
-    def __init__(self, column, row, screen, dots, colour_number=None):
+    def __init__(self, column, row, screen, dots, exclude_this_colour=None):
         pg.sprite.Sprite.__init__(self)
         self.dots = dots  # 2 dimensional list
         self.row = row
         self.column = column
         # короче фишка в том, что если ты срезал цикл некоего цвета, то после этого не должны спавниться кружочки этого цвета
-        if colour_number is not None:
-            self.colour_number = colour_number
-            a = COLOUR_LIST.copy()
-            a.pop(self.colour_number)
-            print(a)
-            self.colour = choice(a)
-        else:
-            self.colour_number = randint(0, 4)
+        if exclude_this_colour is not None:
+            numbers = [i for i in range(len(COLOUR_LIST))]
+            numbers.pop(exclude_this_colour)
+            self.colour_number = choice(numbers)
             self.colour = COLOUR_LIST[self.colour_number]
-
+        else:
+            self.colour_number = randint(0, len(COLOUR_LIST) - 1)
+            self.colour = COLOUR_LIST[self.colour_number]
         # drawing to screen
         self.screen = screen
         self.surface = pg.Surface((50, 50))
@@ -44,14 +43,12 @@ class Dot(pg.sprite.Sprite):
         self.rect = self.surface.get_rect(left=(self.column * 100 + (SCREEN_WIDTH - 550) / 2), top=(self.row + 2) * 100)
         pg.draw.circle(self.surface, self.colour, center=(25, 25), radius=25)
 
-    def pop(self, loop=False):
+    def pop(self, in_loop=False):
         self.surface.fill(BG_COLOR)
         for i in range(self.row-1, -1, -1):  # to maintain order of drop()
             self.dots[self.column][i].drop()
-        if loop:
+        if in_loop:
             # print("AVOID", self.colour_number)
             self.dots[self.column][0] = Dot(self.column, 0, self.screen, self.dots, self.colour_number)
         else:
             self.dots[self.column][0] = Dot(self.column, 0, self.screen, self.dots)
-        # self.screen.blit(self.surface, self.rect)
-        # need to figure out: drop then another while animation
